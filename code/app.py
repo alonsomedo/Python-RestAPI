cf7dc582e9bc1b18b1b448b60ad10c3a55bc4653
AlonsoMD <alonso2295@hotmail.com>
2019-12-08 18:39:35 -0500

    Improve code

--- code/app.py
+++ code/app.py
@@ -1,13 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
+items = []
 
-api.add_resource(Student, '/student/<string:name>')
+class Item(Resource):
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None) # next give us the first item matched 
+        return {'item': item} , 200 if item else 404
+
+    def post(self,name):
+        #Error control if user add a name that already exists
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
+        data = request.get_json() #force = True to transform content type to json, silent = True it doesnt give an error return none
+        item = {'name': name, 'price':data["price"]}
+        items.append(item)
+        return item, 201
+
+class ItemList(Resource):
+    def get(self):
+        return {'items':items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
 app.run(debug=True, port=5000)
+
